d4ae88fea5312fb525ee7f50655b221a442a9e89
kolkatazustina-stack <kolkatazustina@gmail.com>
2025-08-25 18:29:48 +0530

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,6 @@
+
+const CACHE='zustina-v7';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))); self.skipWaiting();});
+self.addEventListener('activate',e=>{e.waitUntil(self.clients.claim());});
+self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
